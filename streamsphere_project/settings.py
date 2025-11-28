020fadafcec4ee3689e0c8d57e2585ca72faffac
EC2 Default User <ec2-user@ip-172-31-72-50.ec2.internal>
2025-11-28 12:30:52 +0000

    S3 support completed

--- streamsphere_project/settings.py
+++ streamsphere_project/settings.py
@@ -56,11 +56,15 @@ ROOT_URLCONF = 'streamsphere_project.urls'
 INSTALLED_APPS += ['storages']
 
 AWS_STORAGE_BUCKET_NAME = 'streamsphere-images'
-AWS_S3_REGION_NAME = 'eu-west-1'  # adjust to your region
+AWS_S3_REGION_NAME = 'us-east-1'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+
 
 TEMPLATES = [
     {
